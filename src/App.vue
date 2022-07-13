5f6c15341541ed3323cd120fb29369e7c24098af
iamppz <iamppz@hotmail.com>
2022-07-13 21:44:35 +0800

    chore: upgrade flowchart-vue to 0.21.0

--- src/App.vue
+++ src/App.vue
@@ -33,24 +33,109 @@ export default {
   data: function () {
     return {
       nodes: [
-        // Basic fields
-        {id: 1, x: 140, y: 270, name: 'Start', type: 'start'},
-        // You can add any generic fields to node, for example: description
-        // It will be passed to @save, @editnode
-        {id: 2, x: 540, y: 270, name: 'End', type: 'end', description: 'I\'m here'},
+        {
+          id: 1,
+          x: 50,
+          y: 220,
+          name: "Start",
+          type: "start",
+          bodyBackgroundColor() {
+            return 'green';
+          },
+          titleBackgroundColor() {
+            return 'green';
+          },
+          borderColor() {
+            return 'red';
+          },
+          textColor(isSelected) {
+            return isSelected ? 'gray' : 'blue';
+          }
+        },
+        {id: 2, x: 630, y: 220, name: "End", type: "end"},
+        {
+          id: 3,
+          x: 340,
+          y: 130,
+          name: "Custom size",
+          type: "operation",
+          approvers: [{id: 1, name: "Joyce"}],
+          width: 120,
+          height: 40,
+          bodyBackgroundColor() {
+            return 'green';
+          },
+          titleBackgroundColor() {
+            return 'green';
+          },
+          borderColor() {
+            return 'red';
+          },
+          textColor(isSelected) {
+            return isSelected ? 'gray' : 'blue';
+          }
+        },
+        {
+          id: 4,
+          x: 240,
+          y: 220,
+          name: "Operation",
+          type: "operation",
+          approvers: [{id: 2, name: "Allen"}],
+        },
+        {
+          id: 5,
+          x: 440,
+          y: 220,
+          name: "Operation",
+          type: "operation",
+          approvers: [{id: 3, name: "Teresa"}],
+        },
       ],
       connections: [
         {
-          source: {id: 1, position: 'right'},
-          destination: {id: 2, position: 'left'},
+          source: {id: 1, position: "right"},
+          destination: {id: 4, position: "left"},
           id: 1,
-          type: 'pass',
+          type: "pass",
+        },
+        {
+          source: {id: 4, position: "right"},
+          destination: {id: 5, position: "left"},
+          id: 2,
+          type: "pass",
+        },
+        {
+          source: {id: 5, position: "right"},
+          destination: {id: 2, position: "left"},
+          id: 3,
+          type: "pass",
+        },
+        {
+          source: {id: 5, position: "bottom"},
+          destination: {id: 4, position: "bottom"},
+          id: 4,
+          type: "reject",
+        },
+        {
+          source: {id: 1, position: "top"},
+          destination: {id: 3, position: "left"},
+          id: 5,
+          type: "pass",
+        },
+        {
+          source: {id: 3, position: "right"},
+          destination: {id: 2, position: "top"},
+          id: 6,
+          type: "pass",
         },
       ],
     };
   },
   methods: {
     handleChartSave(nodes, connections) {
+      console.log(nodes);
+      console.log(connections);
       // axios.post(url, {nodes, connections}).then(resp => {
       //   this.nodes = resp.data.nodes;
       //   this.connections = resp.data.connections;
@@ -63,6 +148,7 @@ export default {
       }
     },
     handleEditConnection(connection) {
+      console.log(connection);
     },
     handleDblClick(position) {
       this.$refs.chart.add({
